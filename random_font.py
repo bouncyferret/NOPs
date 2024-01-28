e657684bd254d7a73ad7d0f914cb4ed711b73d2f
bouncyferret <aslak.k.s@gmail.com>
2024-01-29 00:44:51 +0100

    [UPDATE][#20] Added random font selection on launch

---
+++ random_font.py
@@ -0,0 +1,46 @@
+import random
+from typing import List, Dict, Any, Match
+import re
+import os
+import toml
+
+# if NOPS is not in env -> assume that it's executed from NOPs root dir
+nopsPath: str = os.getenv("NOPS", ".")
+
+# load config to check if random_fonts is enabled
+with open(f"{nopsPath}/nops_config.toml") as file:
+    nopsConfig: Dict[str, Any] = toml.loads(file.read())
+
+if "randomize_fonts" in nopsConfig:
+    nopsRandomizeFonts: bool = nopsConfig["randomize_fonts"]
+else:
+    nopsRandomizeFonts = False
+
+# load font resources template
+templatePath: str = nopsPath + "/config/resources_template"
+with open(templatePath, "r") as file:
+    templateFile: str = file.read()
+
+if nopsRandomizeFonts:
+    print("Ranomizing Fonts")
+
+    fontsCsvPath: str = nopsPath + "/csvs/fonts.csv"
+    with open(fontsCsvPath, "r") as file:
+        fontList: List[str] = file.read().splitlines()
+
+    newConfig: List[str] = []
+    for config in templateFile.splitlines():
+        match: Match[str] = re.search(r"Proportional|Fixed|Black", config)
+        if match:
+            newFont: str = random.choice(fontList)
+            config = config.replace(match.group(), newFont)
+
+        newConfig.append(config + "\n")
+
+    with open("config/resources", "w") as file:
+        file.writelines(newConfig)
+else:
+    print('Restoring "Default" Fonts')
+
+    with open("config/resources", "w") as file:
+        file.writelines(templateFile)
