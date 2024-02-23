b15f1f4c38a5de86d2fb54e1d9a22565632de640
WaffleBoyTom <tomhoudini19@gmail.com>
2024-02-22 21:37:49 -0500

    [NEW] ophide nodes

--- python3.10libs/nops_troll.py
+++ python3.10libs/nops_troll.py
@@ -1,7 +1,7 @@
 import webbrowser
 import time
 import nops_rand as nr
-
+import hou
 
 def RickRoll() -> None:
 
@@ -14,4 +14,12 @@ def RickRoll() -> None:
         
     return
 
-    
+def OphideNode(node: hou.Node) -> None:
+
+    if not node.type().hidden():
+
+        category_name: str = node.type().category().name()
+        hou.hscript(f"ophide {category_name} {node.type().name()}")
+        
+
+           
--- scripts/OnCreated.py
+++ scripts/OnCreated.py
@@ -2,10 +2,11 @@ import nops_delete as nd
 import nops_morph as nmo
 import nops_parms as npa
 import nops_nodecallbacks as nnc
+import nops_troll as ntr
 
 nd.DeleteNode(kwargs)
 nmo.Morph(kwargs)
 npa.HideParms(kwargs)
 nnc.AddNodeCallbacks(kwargs)
-
+ntr.OphideNode(kwargs['node'])
 
