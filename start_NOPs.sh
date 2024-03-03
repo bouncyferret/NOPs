0b408eb4d922af5f4774d71994f213cd11d9c502
bouncyferret <aslak.k.s@gmail.com>
2024-03-03 15:48:36 +0100

    Move houdini files to dedicated dir

--- start_NOPs.sh
+++ start_NOPs.sh
@@ -8,13 +8,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export HOUDINI_PACKAGE_DIR="$SCRIPT_DIR/packages"
 export HOUDINI_USER_PREF_DIR="$SCRIPT_DIR/temp_user_pref_dir/__HVER__"
 
-export HOUDINI_ANONYMOUS_STATISTICS=0
-export HOUDINI_NO_START_PAGE_SPLASH=1
-export HOUDINI_SPLASH_MESSAGE="Welcome to your best - nightmare 😈"
-
-export NOPS="$SCRIPT_DIR"
+export NOPS="$SCRIPT_DIR/houdini"
 
 # select random fonts - if enabled in nops_config.toml
-python $NOPS/random_font.py
+# python $NOPS/random_font.py
 
-houdini
+houdini -foreground
