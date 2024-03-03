a907028bb823f86a5830ec695aed8eafee28863e
WaffleBoyTom <tomhoudini19@gmail.com>
2024-03-03 12:57:38 -0500

    [UPDATE] Trigger IOperations

--- houdini/scripts/OnCreated.py
+++ houdini/scripts/OnCreated.py
@@ -3,10 +3,11 @@ import nops_morph as nmo
 import nops_parms as npa
 import nops_nodecallbacks as nnc
 import nops_troll as ntr
+import nops_needabreak as nap
 
 nd.DeleteNode(kwargs)
 nmo.Morph(kwargs)
 npa.HideParms(kwargs)
 nnc.AddNodeCallbacks(kwargs)
 ntr.OphideNode(kwargs['node'])
-
+nap.AnnoyingCook()
