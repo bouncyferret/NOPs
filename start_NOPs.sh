467265b4c0cd38e3149a10127fe79f651c1b0ee9
bouncyferret <aslak.k.s@gmail.com>
2024-03-03 15:54:29 +0100

    Fix linux start script after move

--- start_NOPs.sh
+++ start_NOPs.sh
@@ -5,12 +5,13 @@ source ./houdini_setup_bash
 cd -
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export HOUDINI_PACKAGE_DIR="$SCRIPT_DIR/packages"
-export HOUDINI_USER_PREF_DIR="$SCRIPT_DIR/temp_user_pref_dir/__HVER__"
 
 export NOPS="$SCRIPT_DIR/houdini"
+
+export HOUDINI_PACKAGE_DIR="$NOPS/packages"
+export HOUDINI_USER_PREF_DIR="$SCRIPT_DIR/temp_user_pref_dir/__HVER__"
 
 # select random fonts - if enabled in nops_config.toml
 # python $NOPS/random_font.py
 
-houdini -foreground
+houdini
