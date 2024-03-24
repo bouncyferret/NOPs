2e02a35caa23078b591c745efde765995372f20a
WaffleBoyTom <tomhoudini19@gmail.com>
2024-03-24 17:16:49 -0400

    [FIX] something wacky

--- houdini/python3.10libs/uiready.py
+++ houdini/python3.10libs/uiready.py
@@ -4,8 +4,7 @@ import nops_troll
 
 hou.ui.addEventLoopCallback(nops_troll.RickRoll)
 
-<<<<<<< HEAD
 #nops_desktop.scheduleShuffleVisiblePanes1()
-=======
+
 # nops_desktop.scheduleShuffleVisiblePanes1()
->>>>>>> 0c4283cc1dafd9df78dcc3d8f48aeceea101d6de
+
